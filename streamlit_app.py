f328dc2079999696e52253b7887493084f145d02
souravaswath31 <souravaswathsouravsavi@gmail.com>
2025-03-16 12:56:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,9 @@ helpful_links = [
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(""" Choose the fruits you want in your custom Smoothie """)
 
+name_on_order = st.text_input("Name on Smoothie")
+st.write('The name of your Smoothie:',name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
