c3b401272c680af682148603844832a3b0f44190
souravaswath31 <souravaswathsouravsavi@gmail.com>
2025-03-16 12:55:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 ingredients_list = st.multiselect(
    'Choose up to 5 ingredients:',
-    my_dataframe
+    my_dataframe,
+    max_selections =5
 )
 
 if ingredients_list:
@@ -31,8 +32,8 @@ if ingredients_list:
 
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
